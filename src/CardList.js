cc32ea01e9dc0d56b2d00009db6551a323b13666
roosleex <roosleex@gmail.com>
2023-04-19 23:48:02 +0300

    Add CardList

---
+++ src/CardList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({robots}) => {
+    const cards = robots.map((user, i) => <Card key={i} id={robots[i].id} name={robots[i].name} email={robots[i].email} />);
+    return (
+        <div>
+            {cards}
+        </div>
+    );
+}
+
+export default CardList;
