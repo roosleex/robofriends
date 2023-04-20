db6451668f654735b117c2ca33d42e241273581f
roosleex <roosleex@gmail.com>
2023-04-20 13:40:51 +0300

    Add loading robots form json query

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React, {Component} from 'react';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
-import {robots} from './robots';
+//import {robots} from './robots';
 import './App.css';
 
 class App extends Component {
     constructor() {
         super();
         this.state = {
-            robots: robots,
+            robots: [],
             searchField: ''
         }
+        // this.state = {
+        //     robots: robots,
+        //     searchField: ''
+        // }
+    }
+
+    componentDidMount() {
+        //console.log('componentDidMount');
+        //this.setState({robots: robots});
+        fetch('https://jsonplaceholder.typicode.com/users')
+            .then(response => response.json())
+            .then(users => this.setState({robots: users}));
     }
 
     onSearchChange = (event) => {
