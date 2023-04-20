afdadacee8f8960c3e5ea25a358f3e0fd80ff7d6
roosleex <roosleex@gmail.com>
2023-04-20 17:55:08 +0300

    Deploy React app to GitHub Pages

---
+++ precache-manifest.8f8142bbc7c4ce9d5f107060f6bc66d5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "d1ec6e9b517476ca83eb",
+    "url": "/robofriends/static/css/main.b42f5eda.chunk.css"
+  },
+  {
+    "revision": "d1ec6e9b517476ca83eb",
+    "url": "/robofriends/static/js/main.d1ec6e9b.chunk.js"
+  },
+  {
+    "revision": "ee2fa2fc286181834050",
+    "url": "/robofriends/static/js/1.ee2fa2fc.chunk.js"
+  },
+  {
+    "revision": "4399e1eabb11a5e1aeed",
+    "url": "/robofriends/static/css/2.191ef14e.chunk.css"
+  },
+  {
+    "revision": "4399e1eabb11a5e1aeed",
+    "url": "/robofriends/static/js/2.4399e1ea.chunk.js"
+  },
+  {
+    "revision": "0e09ba9c7502e90c21a0",
+    "url": "/robofriends/static/js/runtime~main.0e09ba9c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "d2fcbb4e999552f5978edaaa364e0a44",
+    "url": "/robofriends/index.html"
+  }
+];
